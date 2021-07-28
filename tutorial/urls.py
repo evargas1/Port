2a2b782e75530dad8568c451e45d06c307bda1dc
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-28 14:59:07 -0500

    forget to add an include

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from projects import views
 
 urlpatterns = [
+    path('', views.project_index, name='project_index'),
     path('admin/', admin.site.urls),
-    path('', include("projects.urls")),
+    path('', include('projects.urls')),
 ]
 
 
