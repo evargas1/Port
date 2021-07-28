3a0b82f02526f4dc8529166b3c7573308ef739c4
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-28 14:56:20 -0500

    adding normal files

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from projects.models import Project
 # Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'projects/index.html', context)
