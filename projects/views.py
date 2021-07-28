34e441881f37df6a8bc238b443f5a15a29851744
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-28 15:00:51 -0500

    forget to add an include

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,1 @@
 from django.shortcuts import render
-from projects.models import Project
-# Create your views here.
-
-def index(request):
-    context = {}
-    return render(request, 'projects/index.html', context)
